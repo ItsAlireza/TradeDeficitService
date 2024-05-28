9ce8437fb61136a3da7a0a98977f7a2f36cd5c2b
Alireza <salireza.nourbakhsh@gmail.com>
2024-05-28 17:51:19 -0400

    FEATURE [app]: added the DEBUG env variable to differentiate between the internal error displays in dev and prod environments

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -3,6 +3,7 @@ import pandas as pd
 from published.analyzer.inference import inference
 from app.serializers.inference_serializer import InferenceSerializer
 from pydantic import ValidationError
+from . import DEBUG
 
 
 def handler(event, context):
@@ -16,17 +17,27 @@ def handler(event, context):
 
     try:
         inputs_model = InferenceSerializer(**inputs)
-        x = pd.DataFrame([inputs_model.__dict__])  # Directly create DataFrame
+        x = pd.DataFrame([inputs_model.__dict__])
     except ValidationError as e:
-        print(e.json())
+        return {
+            'statusCode': 400,
+            'body': e.json()
+        }
 
-    res = inference(x)
-    result = int(res[0])
+    try:
+        res = inference(x)
+        result = int(res[0])
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'predicted trade deficit is': result})
-    }
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'predicted trade deficit is': result})
+        }
+    except Exception as e:
+        error_message = str(e) if DEBUG else 'Internal Server Error'
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': error_message})
+        }
 
 
 
