56acbed2c60a31a19ab9b66b6a53951b24d72b7e
Alireza <salireza.nourbakhsh@gmail.com>
2024-06-03 12:22:37 -0400

    DEBUG[test]: solved the bug related to the path join before the env. variable check in model_loader.py

--- src/analyzer/model_loader.py
+++ src/analyzer/model_loader.py
@@ -10,15 +10,16 @@ class ModelLoader:
     def load_models(self, config):
         files_path = os.getenv("FILEPATH")
         for model_name, path_var in config.items():
-            path = os.path.join(files_path, os.getenv(path_var))
-
-            if not path:
+            set_path = os.getenv(path_var)
+            if not set_path:
                 raise ValueError(f"Environment variable {path_var} is not set")
 
+            full_path = os.path.join(files_path, set_path)
+
             try:
-                with open(path, 'rb') as f:
+                with open(full_path, 'rb') as f:
                     self.loaded_models[model_name] = pickle.load(f)
             except FileNotFoundError:
-                print(f"File not found for model {model_name} at path {path}")
+                print(f"File not found for model {model_name} at path {full_path}")
             except Exception as e:
                 print(f"An error occurred while loading model {model_name}: {e}")
